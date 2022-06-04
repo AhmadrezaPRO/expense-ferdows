1093f664a2fbb7184469f3d8cb74ebd5aaee0178
AhmadrezaPRO <ahmadrezaprog@gmail.com>
2022-06-04 05:10:32 +0430

    first

--- store/index.js
+++
@@ -1,9 +0,0 @@
-import {formReducer} from "./form-slice";
-
-const {configureStore} = require("@reduxjs/toolkit");
-
-const store = configureStore({
-    reducer: {form: formReducer}
-})
-
-export default store
