de0db630c9a5f6c0430bb0b4f38361c880a72558
AhmadrezaPRO <ahmadrezaprog@gmail.com>
2022-07-13 20:27:19 +0430

    imports

--- components/Main/Table/MyEnhancedTable.js
+++ components/Main/Table/MyEnhancedTable.js
@@ -39,6 +39,10 @@ import FilterForm from "../FilterForm/FilterForm";
 import {Fragment} from "react";
 import {Grow} from "@mui/material";
 import {useCookies} from "react-cookie";
+import {PictureAsPdf} from "@mui/icons-material";
+import jsPDF from 'jspdf';
+import autoTable from "jspdf-autotable";
+import {thousandSeparator} from "../../../utils/formatNumbers";
 
 const useStyles = makeStyles()({
     tableCell: {
