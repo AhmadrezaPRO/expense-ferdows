a8bc6f0dd1f42065c73c7e3cbfea396957a2a751
AhmadrezaPRO <ahmadrezaprog@gmail.com>
2022-07-16 00:52:15 +0430

    down

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ export default function Index({user}) {
 
     const title = 'سامانه تنخواه آزمایشگاه فردوس'
     return (
-        user ? <></> : <Fragment>
+         <Fragment>
             <Head>
                 <title key={'title'}>{title}</title>
                 <meta
