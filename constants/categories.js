716cc023864f5343f81ad70ebf51155ec1113524
AhmadrezaPRO <ahmadrezaprog@gmail.com>
2022-07-29 15:38:59 +0430

    ss

--- constants/categories.js
+++ constants/categories.js
@@ -1,6 +1,6 @@
 const incomeColors = ['#123123', '#154731', '#165f40', '#16784f', '#14915f', '#10ac6e', '#0bc77e', '#04e38d', '#00ff9d'];
 // const expenseColors = ['#b50d12', '#bf2f1f', '#c9452c', '#d3583a', '#dc6a48', '#e57c58', '#ee8d68', '#f79d79', '#ffae8a', '#cc474b', '#f55b5f'];
-const expenseColors = ['#b50d12', '#d3583a', '#FF9966' ,'#DBB2D1', '#dc6a48', '#e57c58', '#ee8d68', '#E68FAC', '#FDBCB4', '#FF69B4', '#f55b5f'];
+const expenseColors = ['#b50d12', '#d3583a', '#FF9966' ,'#DAA520', '#dc6a48', '#e57c58', '#ee8d68', '#E68FAC', '#FDBCB4', '#FF69B4', '#f55b5f'];
 
 export const incomeCategories = [
     {
