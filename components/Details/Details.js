cef1f314a0dcd7de7da3784e77f27bcd0b5634e4
AhmadrezaPRO <ahmadrezaprog@gmail.com>
2022-07-29 15:33:25 +0430

    chart

--- components/Details/Details.js
+++ components/Details/Details.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { Card, CardHeader, CardContent, Typography } from '@mui/material';
+import {Card, CardHeader, CardContent, Typography, Box} from '@mui/material';
 import 'chart.js/auto';
-import { Chart } from 'react-chartjs-2';
+import {Chart} from 'react-chartjs-2';
 
 import useStyles from './style';
 import useTransactions from '../../hooks/useTransactions';
 import NumberFormat from "react-number-format";
+import {thousandSeparator} from "utils/formatNumbers";
 // import List from "../Main/List/List";
 
 const Details = ({title}) => {
     const {classes} = useStyles();
-    const { total, chartData } = useTransactions(title);
+    const {
+        total,
+        chartData
+    } = useTransactions(title);
+    const labels = chartData.labels
+    const amounts = chartData.datasets[0].data
+    const colors = chartData.datasets[0].backgroundColor
+    const mixed = labels.map(function (x, i) {
+        // return [x, amounts[i], colors[i]]
+        return {
+            label: x,
+            amount: amounts[i],
+            color: colors[i]
+        }
+    });
+    const legends = [...mixed].sort((a, b) => b.amount - a.amount);
     const options = {
         plugins: {
             legend: {
+                display: false,
+                rtl: true,
+                position: 'bottom',
                 labels: {
                     font: {
                         family: "Vazirmatn FD" // Add your font here to change the font of your legend label
@@ -32,7 +51,7 @@ const Details = ({title}) => {
     }
     return (
         <Card className={title === 'هزینه' ? classes.expense : classes.income}>
-            <CardHeader title={title} />
+            <CardHeader title={title}/>
             <CardContent>
                 <Typography variant="h5">
                     <NumberFormat
@@ -42,7 +61,20 @@ const Details = ({title}) => {
                         suffix={' تومان'}
                     />
                 </Typography>
-                <Chart type='doughnut' data={chartData} options={options}/>
+                <Chart
+                    type='doughnut'
+                    data={chartData}
+                    options={options}/>
+                {/*{labels.map(label => <div>{label}</div>)}*/}
+                {legends.map(legend =>
+                    <Box sx={{display: 'flex', alignItems: "center"}}>
+                        <Box sx={{
+                            height: '10px',
+                            width: '40px',
+                            backgroundColor: legend.color
+                        }}/>
+                        {`\xa0${legend.label} : ${thousandSeparator(legend.amount.toString())}`}
+                    </Box>)}
             </CardContent>
             {/*<List type={title === 'هزینه' ? 'Expense' : 'Income'} />*/}
         </Card>
